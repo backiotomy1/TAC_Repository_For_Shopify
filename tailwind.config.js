537d8138bc47912bbc10a226f36747ce35a78320
Nick Ledwitz <nickledwitz@gmail.com>
2025-03-25 18:59:05 -0400

    032525 font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        // Global default remains unchanged
         sans: ['"Roboto Condensed"', 'system-ui', 'sans-serif'],
+        // Page titles originally used Oswald; now we want Satoshi for titles too.
+        satoshi: ['"Satoshi"', 'sans-serif']
       },
     },
   },
